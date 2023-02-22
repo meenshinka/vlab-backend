3cedf68c6046c4ceb33232a8bc512c181c23652c
Meenakshi Shukla <92803837+meenshinka@users.noreply.github.com>
2023-02-22 22:09:14 +0530

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -65,7 +65,7 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.comparePassword = async function (yourPassword) {
   return await bcrypt.compare(yourPassword, this.password);
-  this.confirmpassword = await bcrypt.hash(this.confirmpassword, 10);
+  this.cpassword = await bcrypt.hash(this.cpassword, 10);
 };
 
 
