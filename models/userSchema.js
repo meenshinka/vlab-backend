61ad31b990bf971559fa654d56049e7621aeb56e
Meenakshi Shukla <92803837+meenshinka@users.noreply.github.com>
2023-02-22 21:51:19 +0530

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -13,6 +13,9 @@ const userSchema = new mongoose.Schema({
   course: { type: String, required: true },
 
   branch: { type: String, required: true },
+   year: { type: Number, required: true }, 
+
+  semester: { type: Number, required: true },
 
   email: {
     type: String,
