26d9cc037ae9713691afd502b9b5921b79a3ab94
Meenakshi Shukla <92803837+meenshinka@users.noreply.github.com>
2023-02-22 21:51:48 +0530

    Update adminSchema.js

--- models/adminSchema.js
+++ models/adminSchema.js
@@ -13,6 +13,9 @@ const adminSchema = new mongoose.Schema({
   course: { type: String, required: true },
 
   branch: { type: String, required: true },
+   year: { type: Number, required: true }, 
+
+  semester: { type: Number, required: true },
 
   email: {
     type: String,
